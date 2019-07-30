4e000978e2b437999ff3eed6aa86c77bec9591c2
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-07-30 15:53:31 +0200

    Init example chart

--- src/containers/Transactions/Transactions.tsx
+++ src/containers/Transactions/Transactions.tsx
@@ -3,6 +3,7 @@ import {useDispatch, useMappedState} from 'redux-react-hook';
 import { ConseilMetadataClient, ConseilDataClient, ConseilQueryBuilder, ConseilSortDirection, ConseilOperator } from 'conseiljs'
 import * as util from 'util';
 import * as BlokchainActions from "../../store/actions/blokchain";
+import BarChart from "../../components/charts/Bar/Bar";
 
 const platform = 'tezos';
 const network = 'alphanet';
@@ -46,6 +47,8 @@ const mapState = (state: any) => ({
   blokchain: state.blokchain
 });
 
+
+
 const Transactions = () => {
   const dispatch = useDispatch();
   const { blokchain } = useMappedState(mapState);
@@ -60,10 +63,33 @@ const Transactions = () => {
     });
   };
 
+  const chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My example chart',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+
   return (
     <div>
       <h1>Transactions:</h1>
       {blokchain.map((item: any, index: any) => <div key={index}>{item.source}</div>)}
+      <BarChart
+        data={chartData}
+        width={100}
+        height={200}
+        options={{
+        maintainAspectRatio: false
+        }}
+      />
     </div>
   );
 };
