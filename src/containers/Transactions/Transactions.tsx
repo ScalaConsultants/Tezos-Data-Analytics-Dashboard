971c852a33cb0fbec0806fab2873860ad09f4797
Unknown <lordh@wp.pl>
2019-07-30 15:31:04 +0200

    50000 last transactions

--- src/containers/Transactions/Transactions.tsx
+++ src/containers/Transactions/Transactions.tsx
@@ -1,46 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useDispatch, useMappedState} from 'redux-react-hook';
-import { ConseilMetadataClient, ConseilDataClient, ConseilQueryBuilder, ConseilSortDirection, ConseilOperator } from 'conseiljs'
-import * as util from 'util';
 import * as BlokchainActions from "../../store/actions/blokchain";
 
-const platform = 'tezos';
-const network = 'alphanet';
-const entity = 'operations';
-
-const conseilServerInfo = {
-  url: "" + process.env.REACT_APP_CONSEIL_URL,
-  apiKey: "" + process.env.REACT_APP_CONSEIL_KEY
-};
-
-async function listEntities() {
-  const platforms = await ConseilMetadataClient.getEntities(conseilServerInfo, 'tezos', 'alphanet');
-  console.log(`${util.inspect(platforms, false, 2, false)}`);
-}
-
-async function listAccountTransactions() {
-  let sendQuery = ConseilQueryBuilder.blankQuery();
-  sendQuery = ConseilQueryBuilder.addFields(sendQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
-  sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'kind', ConseilOperator.EQ, ['transaction'], false);
-  sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'source', ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
-  sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'status', ConseilOperator.EQ, ['applied'], false);
-  sendQuery = ConseilQueryBuilder.addOrdering(sendQuery, 'block_level', ConseilSortDirection.DESC);
-  sendQuery = ConseilQueryBuilder.setLimit(sendQuery, 100);
-
-  let receiveQuery = ConseilQueryBuilder.blankQuery();
-  receiveQuery = ConseilQueryBuilder.addFields(receiveQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
-  receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'kind', ConseilOperator.EQ, ['transaction'], false);
-  receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'destination', ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
-  receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'status', ConseilOperator.EQ, ['applied'], false);
-  receiveQuery = ConseilQueryBuilder.addOrdering(receiveQuery, 'block_level', ConseilSortDirection.DESC);
-  receiveQuery = ConseilQueryBuilder.setLimit(receiveQuery, 100);
-
-  const sendResult = await ConseilDataClient.executeEntityQuery(conseilServerInfo, platform, network, entity, sendQuery);
-  const receiveResult = await ConseilDataClient.executeEntityQuery(conseilServerInfo, platform, network, entity, receiveQuery);
-  const transactions = sendResult.concat(receiveResult).sort((a, b) => { return a['timestamp'] - b['timestamp'] });
-
-  console.log(`${util.inspect(transactions, false, 2, false)}`);
-}
 
 const mapState = (state: any) => ({
   blokchain: state.blokchain
@@ -63,7 +24,7 @@ const Transactions = () => {
   return (
     <div>
       <h1>Transactions:</h1>
-      {blokchain.map((item: any, index: any) => <div key={index}>{item.source}</div>)}
+      {blokchain.slice(0, 100).map((item: any, index: any) => <div key={index}>{item.timestamp} {item.source} {item.destination} </div>)}
     </div>
   );
 };
--- src/store/sagas/blokchain.ts
+++ src/store/sagas/blokchain.ts
@@ -16,6 +16,31 @@ const fetchTransactionsRequest = async () => {
   let sendQuery = ConseilQueryBuilder.blankQuery();
   sendQuery = ConseilQueryBuilder.addFields(sendQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
   sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'kind', ConseilOperator.EQ, ['transaction'], false);
+  // sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'source', ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
+  sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'status', ConseilOperator.EQ, ['applied'], false);
+  sendQuery = ConseilQueryBuilder.addOrdering(sendQuery, 'block_level', ConseilSortDirection.DESC);
+  sendQuery = ConseilQueryBuilder.setLimit(sendQuery, 50000);
+
+  let receiveQuery = ConseilQueryBuilder.blankQuery();
+  receiveQuery = ConseilQueryBuilder.addFields(receiveQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
+  receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'kind', ConseilOperator.EQ, ['transaction'], false);
+  // receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'destination', ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
+  receiveQuery = ConseilQueryBuilder.addPredicate(receiveQuery, 'status', ConseilOperator.EQ, ['applied'], false);
+  receiveQuery = ConseilQueryBuilder.addOrdering(receiveQuery, 'block_level', ConseilSortDirection.DESC);
+  receiveQuery = ConseilQueryBuilder.setLimit(receiveQuery, 50000);
+
+  const sendResult = await ConseilDataClient.executeEntityQuery(conseilServerInfo, platform, network, entity, sendQuery);
+  const receiveResult = await ConseilDataClient.executeEntityQuery(conseilServerInfo, platform, network, entity, receiveQuery);
+  const transactions = sendResult.concat(receiveResult).sort((a, b) => { return a['timestamp'] - b['timestamp'] });
+
+  let result = `${util.inspect(transactions, false, 2, false)}`;
+  return transactions;
+};
+
+const fetchTransactionsRequest0 = async () => {
+  let sendQuery = ConseilQueryBuilder.blankQuery();
+  sendQuery = ConseilQueryBuilder.addFields(sendQuery, 'block_level', 'timestamp', 'source', 'destination', 'amount', 'fee', 'counter');
+  sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'kind', ConseilOperator.EQ, ['transaction'], false);
   sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'source', ConseilOperator.EQ, ['tz1WpPzK6NwWVTJcXqFvYmoA6msQeVy1YP6z'], false);
   sendQuery = ConseilQueryBuilder.addPredicate(sendQuery, 'status', ConseilOperator.EQ, ['applied'], false);
   sendQuery = ConseilQueryBuilder.addOrdering(sendQuery, 'block_level', ConseilSortDirection.DESC);
