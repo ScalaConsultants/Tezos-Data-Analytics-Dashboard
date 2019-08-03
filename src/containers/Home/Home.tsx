ac6b4eaba4ddb614ce46ff27ebee2168baedaaac
Unknown <lordh@wp.pl>
2019-08-03 09:05:48 +0200

    Refactoring #2

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h2>Welcome to Tezos Data Analytics Dashboard</h2>
+      <h1>Welcome to Tezos Data Analytics Dashboard</h1>
+      <h2>General idea:</h2>
+      <p>1. Technology stack: React + Typescript + Hooks + Redux + Saga + ConseilJS.</p>
+      <p>2. We get a transaction list (25000) only once and save it to the cache. To reach this we created a simple 'local storage' based cache.</p>
+      <p>3. Every 10 seconds we are getting a list of the latest transactions and add them to the cache.</p>
+      <p>4. We are creating charts for data stored in the cache.</p>
+      <p>5. Be forgiving, please - time range was limited.</p>
     </div>
   );
 };
