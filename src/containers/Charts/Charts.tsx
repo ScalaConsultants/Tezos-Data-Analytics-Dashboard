791714377f4abff7434c624109bdda00793f46e6
Wojciech Nowakowski <wojciech.nowakowski94@gmail.com>
2019-08-04 10:52:09 +0200

    Fix day of time

--- src/containers/Charts/Charts.tsx
+++ src/containers/Charts/Charts.tsx
@@ -37,8 +37,8 @@ const getDayTime = (hour: any) => {
   if (hour > 12 && hour < 18) {
     return 'afternoon';
   }
-  if (hour > 18 && hour < 24) {
-    return 'afternoon';
+  if (hour > 18 && hour < 23) {
+    return 'evening';
   }
   if (hour > 0 && hour < 6) {
     return 'night';
